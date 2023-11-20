8058f36237dad2e0f6a77b7c77d90ffe4a8ba93a
EJVargas03 <143569160+EJVargas03@users.noreply.github.com>
2023-11-19 20:58:47 -0600

    index changes

---
+++ src/routes/api/index.js
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+
+import playlists from './playlists'
+
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.send({ msg: 'Inside API Endpoints' })
+  })
+
+router.use('/playlists', playlists)
+
+  export default router
