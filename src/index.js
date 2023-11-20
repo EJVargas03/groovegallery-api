78b52b6b7e0160b1fbe1e9ea8ca40bb3f2b0a2a9
EJVargas03 <143569160+EJVargas03@users.noreply.github.com>
2023-11-19 22:06:30 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
-import bodyParser from 'body-parser'
 
 import logger from './utils/logger'
 import router from './routes'
@@ -15,14 +14,8 @@ const port = Number(process.env.PORT)
 const app = express()
 
 app.use(morgan(process.env.MORGAN_LOG))
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    exposedHeaders: ['x-total-count', 'x-total-pages'],
-  }),
-)
+app.use(cors({ origin: process.env.CORS_ORIGIN }))
 app.use(helmet())
-app.use(bodyParser.json())
 
 app.use('/', router)
 
@@ -30,4 +23,5 @@ app.use(notFound)
 app.use(errorHandler)
 
 app.listen(port, () => {
-  logger.info(`Server running on port ${port}`)})
+  logger.info(`Server running on port ${port}`)
+})
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,13 +1,9 @@
 import { Router } from 'express'
 
-import playlists from './playlists'
-
 const router = Router()
 
 router.get('/', (req, res) => {
     res.send({ msg: 'Inside API Endpoints' })
   })
-
-router.use('/playlists', playlists)
 
   export default router
